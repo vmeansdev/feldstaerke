b954c0f2a48b1189cd02b00d185d9aff5acf12cc
vmeansdev <vmeansdev@gmail.com>
2017-12-12 15:04:05 +0300

    UPDATE: added bot config as env vars of app config, added logs to main app module

--- src/feldstaerke_app.erl
+++ src/feldstaerke_app.erl
@@ -6,6 +6,7 @@
 -module(feldstaerke_app).
 
 -behaviour(application).
+-include("feldstaerke.hrl").
 
 %% Application callbacks
 -export([start/2, stop/1]).
@@ -14,11 +15,15 @@
 %% API
 %%====================================================================
 
+-spec start(_, _) -> {ok, pid()}.
 start(_StartType, _StartArgs) ->
+    log:info(?MFN, "app started"),
     feldstaerke_sup:start_link().
 
 %%--------------------------------------------------------------------
+-spec stop(_) -> ok.
 stop(_State) ->
+    log:info(?MFN, "app stopped"),
     ok.
 
 %%====================================================================
