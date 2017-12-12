1efeddaa21e3e4fb62974cc6afdbca8ece3451c3
vmeansdev <vmeansdev@gmail.com>
2017-12-12 15:05:22 +0300

    ADD: cowboy handler

---
+++ src/bot_cowboy_handler.erl
@@ -0,0 +1,27 @@
+%%%-------------------------------------------------------------------
+%%% @author vmeansdev
+%%% @copyright (C) 2017, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 11. Dec 2017 22:44
+%%%-------------------------------------------------------------------
+-module(bot_cowboy_handler).
+-author("vmeansdev").
+
+-include("feldstaerke.hrl").
+
+%% API
+-export([init/2]).
+
+-spec init(cowboy_req:req(), term()) -> {ok, cowboy_req:req(), term()}.
+init(Req0, State) ->
+    {ok, Data, _} = cowboy_req:read_body(Req0),
+    Req = cowboy_req:reply(200,
+        #{}, %% Headers
+        "" , %% Body
+        Req0),
+    log:info(?MFN, "Init called, now call parser parse_msg..."),
+    feldstaerke_parser:parse_msg(Data),
+
+    {ok, Req, State}.
