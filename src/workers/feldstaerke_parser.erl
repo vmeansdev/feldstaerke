704060be9b363232622a31d7b9f27cd4e8cdb6d5
vmeansdev <vmeansdev@gmail.com>
2017-12-20 20:55:47 +0300

    UPDATE: parser is not a gen_server anymore, simple module is enough for such a functionality

--- src/workers/feldstaerke_parser.erl
+++ src/workers/feldstaerke_parser.erl
@@ -9,97 +9,17 @@
 -module(feldstaerke_parser).
 -author("vmeansdev").
 
--behaviour(gen_server).
-
 -include("../headers/feldstaerke.hrl").
 
 %% API
--export([start_link/0,
-         parse_msg/1]).
-
-%% gen_server callbacks
--export([init/1,
-         handle_call/3,
-         handle_cast/2,
-         handle_info/2,
-         terminate/2,
-         code_change/3]).
-
--define(SERVER, ?MODULE).
-
--record(state, {}).
+-export([parse_msg/1]).
 
 %%%===================================================================
 %%% API
 %%%===================================================================
 
-%%--------------------------------------------------------------------
-%% @doc
-%% Starts the server
-%%
-%% @end
-%%--------------------------------------------------------------------
--spec(start_link() ->
-    {ok, Pid :: pid()} | ignore | {error, Reason :: term()}).
-start_link() ->
-    gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
-
 -spec parse_msg(binary()) -> ok.
 parse_msg(Msg) ->
-    gen_server:cast(?SERVER, Msg).
-
-%%%===================================================================
-%%% gen_server callbacks
-%%%===================================================================
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Initializes the server
-%%
-%% @spec init(Args) -> {ok, State} |
-%%                     {ok, State, Timeout} |
-%%                     ignore |
-%%                     {stop, Reason}
-%% @end
-%%--------------------------------------------------------------------
--spec(init(Args :: term()) ->
-    {ok, State :: #state{}} | {ok, State :: #state{}, timeout() | hibernate} |
-    {stop, Reason :: term()} | ignore).
-init([]) ->
-    log:info(?MFN, "Start incoming messages parser"),
-    {ok, #state{}}.
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Handling call messages
-%%
-%% @end
-%%--------------------------------------------------------------------
--spec(handle_call(Request :: term(), From :: {pid(), Tag :: term()},
-    State :: #state{}) ->
-    {reply, Reply :: term(), NewState :: #state{}} |
-    {reply, Reply :: term(), NewState :: #state{}, timeout() | hibernate} |
-    {noreply, NewState :: #state{}} |
-    {noreply, NewState :: #state{}, timeout() | hibernate} |
-    {stop, Reason :: term(), Reply :: term(), NewState :: #state{}} |
-    {stop, Reason :: term(), NewState :: #state{}}).
-handle_call(_Request, _From, State) ->
-    {reply, ok, State}.
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Handling cast messages
-%%
-%% @end
-%%--------------------------------------------------------------------
--spec(handle_cast(Request :: term(), State :: #state{}) ->
-    {noreply, NewState :: #state{}} |
-    {noreply, NewState :: #state{}, timeout() | hibernate} |
-    {stop, Reason :: term(), NewState :: #state{}}).
-handle_cast(Msg, _) ->
     log:info(?MFN, "Message received"),
     log:debug(?MFN, Msg),
     UpdateBody  = jsx:decode(Msg),
@@ -116,59 +36,11 @@ handle_cast(Msg, _) ->
             handle_message_body(Inf);
         <<"callback_query">> ->
             io:format("MsgType matched to <<callback_query>>~n"),
-            handle_cb_query(Inf);
+            handle_callback_query(Inf);
         _ ->
             io:format("MsgType matched to *~n"),
             validate_message(true, 0)
-    end,
-    {noreply, {}}.
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Handling all non call/cast messages
-%%
-%% @spec handle_info(Info, State) -> {noreply, State} |
-%%                                   {noreply, State, Timeout} |
-%%                                   {stop, Reason, State}
-%% @end
-%%--------------------------------------------------------------------
--spec(handle_info(Info :: timeout() | term(), State :: #state{}) ->
-    {noreply, NewState :: #state{}} |
-    {noreply, NewState :: #state{}, timeout() | hibernate} |
-    {stop, Reason :: term(), NewState :: #state{}}).
-handle_info(_Info, State) ->
-    {noreply, State}.
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% This function is called by a gen_server when it is about to
-%% terminate. It should be the opposite of Module:init/1 and do any
-%% necessary cleaning up. When it returns, the gen_server terminates
-%% with Reason. The return value is ignored.
-%%
-%% @spec terminate(Reason, State) -> void()
-%% @end
-%%--------------------------------------------------------------------
--spec(terminate(Reason :: (normal | shutdown | {shutdown, term()} | term()),
-    State :: #state{}) -> term()).
-terminate(_Reason, _State) ->
-    ok.
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Convert process state when code is changed
-%%
-%% @spec code_change(OldVsn, State, Extra) -> {ok, NewState}
-%% @end
-%%--------------------------------------------------------------------
--spec(code_change(OldVsn :: term() | {down, term()}, State :: #state{},
-    Extra :: term()) ->
-    {ok, NewState :: #state{}} | {error, Reason :: term()}).
-code_change(_OldVsn, State, _Extra) ->
-    {ok, State}.
+    end.
 
 %%%===================================================================
 %%% Internal functions
@@ -187,7 +59,7 @@ handle_message_body(Body) ->
 
     validate_message(ParseFailed, Reply).
 
-handle_cb_query(Body) ->
+handle_callback_query(Body) ->
     Query     = get_value(<<"callback_query">>, Body),
     UserId    = get_value(<<"id">>, get_value(<<"from">>, Query)),
     Username  = get_value(<<"username">>, get_value(<<"from">>, Query)),
